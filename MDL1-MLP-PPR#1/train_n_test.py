ad2792b7fb345abfb98390ef5c763462342679fd
Akshay Raina <rains.akshay@gmail.com>
2021-01-28 22:15:21 +0530

    finalised train_n_tst.py

--- MDL1-MLP-PPR#1/train_n_test.py
+++ MDL1-MLP-PPR#1/train_n_test.py
@@ -1,22 +1,28 @@
 def train(kf, n=0.2, k=10):
-    import pandas as pd
     from sklearn.neural_network import MLPClassifier as mlp
-    from sklearn.metrics import confusion_matrix, accuracy_score
     from datahandle import datasetpartition as dsp
-    from sklearn.model_selection import KFold
-    from sklearn.model_selection import cross_val_score
-    import statistics as st    
-    try:
+    try: 
         X_train, X_test, y_train, y_test=dsp(part=kf,m=n,f=k)
+       #TRAINING
         clf= mlp(solver='sgd', hidden_layer_sizes=(200,), random_state=1, max_iter=1000)
         clf.fit(X_train, y_train)
         print('\n Multilayer Perceptron Network trained with sgd solver!!')
+       #TESTING
+        predictions=clf.predict(X_test)
     except:
-        splt=dsp(part=kf,m=n,f=k)
+        X,y,splt=dsp(part=kf,m=n,f=k)
+        predictions={}; y_test={}; i=1;     
         for trn_id, tst_id in splt:
             X_trn, X_tst, y_trn, y_tst = X[trn_id], X[tst_id] ,\
                                          y[trn_id], y[tst_id]
-        clf= mlp(solver='sgd', hidden_layer_sizes=(200,), random_state=1, max_iter=1000)
-        clf.fit(X_trn, y_trn)
+           #TRAINING
+            clf= mlp(solver='sgd', hidden_layer_sizes=(200,), random_state=1, max_iter=1000)
+            clf.fit(X_trn, y_trn)
+            print('\n Multilayer Perceptron Network trained with sgd solver!!')
+           #TESTING
+            pred=clf.predict(X_tst)
+            predictions[i]=pred; y_test[i]=y_tst; i+=1;
+    finally:
+        return y_test,predictions
         
-        
+    
