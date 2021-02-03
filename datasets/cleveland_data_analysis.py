da8447f7e7f1e0d3fb568c796e6a8740ac01dc9d
Shubam Sumbria <shubamsumbria66@gmail.com>
2021-02-03 21:38:23 +0530

    update cleveland_data_analysis.py

--- datasets/cleveland_data_analysis.py
+++ datasets/cleveland_data_analysis.py
@@ -28,11 +28,11 @@ df['sex'] = df.sex.map({'female': 0, 'male': 1})
 
 #ANALYSING VARIOUS STATISTICAL MEASURES OF FEATURES AND INTRA- FEATURES; [Mean, Std, Min, Max etc.]
 pd.set_option("display.float", "{:.2f}".format)
-dfx = dataframe.drop(columns=["sex","target"])
+dfx = df.drop(columns=["sex","target"])
 dfx.describe()
 
 #Plot4(No. of people with heart disease vs No. of people without heart disease)
-dataframe.target.value_counts().plot(kind="bar",width=0.1,color=["salmon","lightgreen"],legend=1,figsize=(8,5))
+df.target.value_counts().plot(kind="bar",width=0.1,color=["salmon","lightgreen"],legend=1,figsize=(8,5))
 plt.ylabel("No. of People", fontsize=12)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
@@ -40,22 +40,22 @@ plt.legend(["No. of people with heart disease"],fontsize=12);
 plt.show()
 
 #Plot5(Correlation Matirx Heatmap)
-corr_matrix = dataframe.corr()
+corr_matrix = df.corr()
 fig, ax = plt.subplots(figsize=(22, 10))
 ax = sb.heatmap(corr_matrix,annot=True,linewidths=0.5,fmt=".2f",cmap="YlGn");
 bottom, top = ax.get_ylim()
 ax.set_ylim(bottom + 0.5, top - 0.5);
 
 #Plot6(Correlation with Target)
-dataframe.drop('target', axis=1).corrwith(dataframe.target).plot(kind='bar', grid=True, figsize=(20, 8), title="Correlation with target",color="lightgreen");
+df.drop('target', axis=1).corrwith(df.target).plot(kind='bar', grid=True, figsize=(20, 8), title="Correlation with target",color="lightgreen");
 
 #Plot7(Categorical and Continous Values)
 categorical_val = []
 continous_val = []
-for column in dataframe.columns:
+for column in df.columns:
     print('-------------------------------')
-    print(f"{column} : {dataframe[column].unique()}")
-    if len(dataframe[column].unique()) <= 10:
+    print(f"{column} : {df[column].unique()}")
+    if len(df[column].unique()) <= 10:
         categorical_val.append(column)
     else:
         continous_val.append(column)
@@ -64,8 +64,8 @@ for column in dataframe.columns:
 plt.figure(figsize=(20, 16))
 for i, column in enumerate(categorical_val, 1):
     plt.subplot(3, 3, i)
-    dataframe[dataframe["target"] == 1][column].hist(bins=35, color='red', label='People with heart disease',alpha=0.8)
-    dataframe[dataframe["target"] == 0][column].hist(bins=35, color='green', label='People without heart disease',alpha=0.5)
+    df[df["target"] == 1][column].hist(bins=35, color='red', label='People with heart disease',alpha=0.8)
+    df[df["target"] == 0][column].hist(bins=35, color='green', label='People without heart disease',alpha=0.5)
     plt.legend(fontsize=12)
     plt.xlabel(column)
     plt.ylabel("No. of People")
@@ -74,16 +74,16 @@ for i, column in enumerate(categorical_val, 1):
 plt.figure(figsize=(20, 16))
 for i, column in enumerate(continous_val, 1):
     plt.subplot(3, 2, i)
-    dataframe[dataframe["target"] == 1][column].hist(bins=35, color='red', label='People with heart disease', alpha=0.8)
-    dataframe[dataframe["target"] == 0][column].hist(bins=35, color='green', label='People without heart disease',alpha=0.5)
+    df[df["target"] == 1][column].hist(bins=35, color='red', label='People with heart disease', alpha=0.8)
+    df[df["target"] == 0][column].hist(bins=35, color='green', label='People without heart disease',alpha=0.5)
     plt.legend(fontsize=12)
     plt.xlabel(column)
     plt.ylabel("No. of People")
 
 #Plot10(Scatter Plot(Age Vs Max Heart Rate))
 plt.figure(figsize=(15, 10))
-plt.scatter(dataframe.age[dataframe.target==1],dataframe.thalach[dataframe.target==1],c="red",s=75)
-plt.scatter(dataframe.age[dataframe.target==0],dataframe.thalach[dataframe.target==0],c="green",alpha=0.5)
+plt.scatter(df.age[df.target==1],df.thalach[df.target==1],c="red",s=75)
+plt.scatter(df.age[df.target==0],df.thalach[df.target==0],c="green",alpha=0.5)
 plt.title("Heart Disease in function of Age and Max Heart Rate",fontsize=14)
 plt.xlabel("Age", fontsize=14)
 plt.ylabel("Max Heart Rate", fontsize=14)
@@ -91,8 +91,8 @@ plt.legend(["Disease", "No Disease"],fontsize=18);
 
 #Plot11(Scatter Plot(Age Vs Serum Cholestoral(mg/dl))); Alternate to Plot1
 plt.figure(figsize=(15, 10))
-plt.scatter(dataframe.age[dataframe.target==1],dataframe.chol[dataframe.target==1],c="red",s=75)
-plt.scatter(dataframe.age[dataframe.target==0],dataframe.chol[dataframe.target==0],c="green",alpha=0.5)
+plt.scatter(df.age[df.target==1],df.chol[df.target==1],c="red",s=75)
+plt.scatter(df.age[df.target==0],df.chol[df.target==0],c="green",alpha=0.5)
 plt.title("Heart Disease in function of Age and Serum Cholestoral(mg/dl)",fontsize=14)
 plt.xlabel("Age", fontsize=14)
 plt.ylabel("Serum Cholestoral(mg/dl)", fontsize=14)
@@ -100,8 +100,8 @@ plt.legend(["Disease", "No Disease"],fontsize=18);
 
 #Plot12(Scatter Plot(Age Vs Resting Blood Pressure))
 plt.figure(figsize=(15, 10))
-plt.scatter(dataframe.age[dataframe.target==1],dataframe.trestbps[dataframe.target==1],c="red",s=75)
-plt.scatter(dataframe.age[dataframe.target==0],dataframe.trestbps[dataframe.target==0],c="green",alpha=0.5)
+plt.scatter(df.age[df.target==1],df.trestbps[df.target==1],c="red",s=75)
+plt.scatter(df.age[df.target==0],df.trestbps[df.target==0],c="green",alpha=0.5)
 plt.title("Heart Disease in function of Age and Resting Blood Pressure",fontsize=14)
 plt.xlabel("Age", fontsize=14)
 plt.ylabel("Resting Blood Pressure", fontsize=14)
