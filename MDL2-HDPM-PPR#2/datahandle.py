5e6d2b78bfa2b8c823930f5f865ee9c42cb4899d
raina-akshay <18bee003@smvdu.ac.in>
2021-02-09 23:43:45 +0530

    minor changes

--- MDL2-HDPM-PPR#2/datahandle.py
+++ MDL2-HDPM-PPR#2/datahandle.py
@@ -1,15 +1,17 @@
-def convert_data():
+def convert_data(): #searches for data, convertes it if not in csv format and then returns a DF
     import os
     from pathlib import Path
-    
+    import pandas as pd
     if os.path.exists("statlog.csv"):
-        print('dataset found')
+        print('dataset found in current directory')
+        return pd.read_csv('statlog.csv')
     else:
         print('dataset not found. Now changing cwd to search for it. will return back here if found in datasets folder')
         p = Path(os.getcwd())
         os.chdir(str(p.parent) + '\datasets')
         if os.path.exists("statlog.csv"):
-            print('dataset found')
+            print('dataset found in /datasets sub- directory')
+            return pd.read_csv('statlog.csv')
         elif os.path.exists('statlog.dat'):
             import csv
             data_fldr = os.getcwd()
@@ -35,7 +37,23 @@ def convert_data():
                                  'slope', 'ca', 'thal', 'presence'])
                 for r in new_data:
                     writer.writerow(r)
+            return pd.read_csv('statlog.csv')
         else:
             print('There is no file corresponding to the Statlog dataset, be it in .csv or .dat format')
         p = Path(os.getcwd())
         os.chdir(str(p.parent) + '\MDL2-HDPM-PPR#2')
+        
+def fs():
+    from sklearn.feature_selection import mutual_info_classif as mic
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    data=convert_data(); X=data.iloc[:,0:13]; y=data.iloc[:,13]
+    imp=mic(X,y)
+    feat_imp=pd.Series(imp)
+    feat_imp.plot(kind='barh',color='teal')
+    plt.show()
+    return data
+
+fs()
+#in case of ':' indexing, second digit isnt included...but in case of it as single number, it is the number it is, i.e. if the number is 'n' u get the col no. 'n+1'
+    
